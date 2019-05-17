1abdff0a4159744e531e8615ac07ed700cde6d7b
Michał Gołdys <michal.goldys@onet.pl>
2019-05-17 13:57:02 +0200

    * Update - Removed ResourceHandler

--- src/main/java/pl/michalgoldys/zarzadzaniepracownikami/ApplicationWebMvcConfig.java
+++ src/main/java/pl/michalgoldys/zarzadzaniepracownikami/ApplicationWebMvcConfig.java
@@ -15,6 +15,6 @@ public class ApplicationWebMvcConfig implements WebMvcConfigurer{
 	public void addResourceHandlers(final ResourceHandlerRegistry registry) {
 	        registry.addResourceHandler("/images/**").addResourceLocations("/resources/images/");
 	        registry.addResourceHandler("/css/**").addResourceLocations("/resources/css/");
-	        registry.addResourceHandler("/teamplates/**").addResourceLocations("/resources/teamplates/");
+
 	    }
 }
