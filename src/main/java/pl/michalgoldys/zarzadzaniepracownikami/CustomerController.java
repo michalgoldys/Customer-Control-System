6df9c1ae600af262c1f8b6325b803f37a9558591
Michał Gołdys <michal.goldys@onet.pl>
2019-05-17 13:59:04 +0200

    * Update - Moved database methods to service layer

--- src/main/java/pl/michalgoldys/zarzadzaniepracownikami/CustomerController.java
+++ src/main/java/pl/michalgoldys/zarzadzaniepracownikami/CustomerController.java
@@ -1,8 +1,5 @@
 package pl.michalgoldys.zarzadzaniepracownikami;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,37 +12,25 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class CustomerController {
-	
-		@Autowired
-		CustomerRepository customerRepository;
-		
-		@Autowired
-		CustomerRepositoryImp customerRepositoryImp;
-		
-		@Autowired
-		CustomerAdressRepository customerAdressRepository;
-		
-		@Autowired
-		CustomerContactRepository customerContactRepository;
-	
+
 		@Autowired 
-		CustomerDatabaseService customerDatabaseService;
+		private CustomerDatabaseService customerDatabaseService;
 		
 		@GetMapping(value= "/customer/customerMenu")
-		public String customerMenu() {
+		private String customerMenu() {
 			return "customerMenu";
 		}
 		
 		@GetMapping(value="/customer/showingCustomers")
-		public String showingCustomers(Model model) {
+		private String showingCustomers(Model model) {
 			
-			model.addAttribute("customer", customerRepository.findAll());
+			model.addAttribute("customer", customerDatabaseService.findAllCustomers());
 			
 			return "showingCustomers";
 		}
 		
 		@GetMapping(value="/customer/addingCustomer")
-		public String addingCustomersForm(
+		private String addingCustomersForm(
 				Customer customer, CustomerAdress customerAdress, CustomerContact customerContact, CustomerContractDetalis customerContractDetalis,
 				CustomerDTO customerDto, CustomerAdressDTO customerAdressDto, CustomerContactDTO customerContactDto, 
 				CustomerContractDetalisDTO customerContractDetalisDto) {
@@ -54,7 +39,7 @@ public class CustomerController {
 		}
 		
 		@PostMapping(value="/customer/addingCustomer")
-		public String addingCustomers(
+		private String addingCustomers(
 				@Valid CustomerDTO customerDto, BindingResult customerBinding,
 				@Valid CustomerAdressDTO customerAdressDto, BindingResult customerBinding1,
 				@Valid CustomerContactDTO customerContactDto, BindingResult customerBinding2, 
@@ -64,94 +49,31 @@ public class CustomerController {
 			if (customerBinding.hasErrors()) {
 					return "addingCustomer";
 				}
-			
-			customerAdress.setCustomer(customer);
-			customer.setCustomerAdress(customerAdress);
-			customerContact.setCustomer(customer);
-			
-			
-			List<CustomerContact> customerContactList = new ArrayList<CustomerContact>();
-			customerContactList.add(customerContact);
-			
-			customer.setCustomerContact(customerContactList);
-			
-			
-			customerContractDetalis.setCustomer(customer);
-			customer.setCustomerContractDetalis(customerContractDetalis);
-			
-			customerRepository.save(customer);
+
+			customerDatabaseService.creatingCustomer(customer, customerAdress, customerContact, customerContractDetalis);
 
 			return "redirect:/customer/customerMenu";
 			
 		}
 		
 		@GetMapping(value="/customer/showingCustomers/customerDetalis")
-		public String showingCustomerDetalis(@RequestParam("id") String customerSelectionId, Model model,
+		private String showingCustomerDetalis(@RequestParam("id") String customerSelectionId, Model model,
 				Customer customer, CustomerAdress customerAdress, CustomerContact customerContact, CustomerContractDetalis customerContractDetalis
 				) {
 			
 			model.addAttribute("selectedCustomerId", customerSelectionId);
-			model.addAttribute("selectedCustomerById", customerRepository.findBycustomerContractPdfId(customerSelectionId));
+			model.addAttribute("selectedCustomerById", customerDatabaseService.listFindByCustomerContractPdfId(customerSelectionId));
 			
 			return "customerDetalis";
 		}
-
 		
 		@PostMapping(value="/customer/showingCustomers/customerDetalis")
-		public String settingCustomerDetalis(@RequestParam("id") String customerSelectionId, 
+		private String settingCustomerDetalis(@RequestParam("id") String customerSelectionId, 
 				Customer customer, CustomerAdress customerAdress, CustomerContact customerContact, CustomerContractDetalis customerContractDetalis, 
 				Model model
 				) {
-			/*
-			Customer customerToUpdate = new Customer();
-			customerToUpdate = customerRepositoryImp.findBycustomerContractPdfId(customerSelectionId);
 			
-			customerToUpdate.setCustomerAdress(customerAdress);
-			customerToUpdate.setCustomerContractDetalis(customerContractDetalis);
-			customerToUpdate.setCustomerNip(customer.getCustomerNip());
-			customerToUpdate.setCustomerName(customer.getCustomerName());
-			
-			//Customer customerPdfIdUpdate = customerRepositoryImp.findBycustomerContractPdfId(customerSelectionId);
-			//Long customerId = customerPdfIdUpdate.getCustomerId();
-			
-			//Customer customerUpdate = customerRepositoryImp.getOne(customerId);
-			
-			// CustomerAdress customerAdressUpate = customerAdressRepository.findBycustomerAdressId(customerId);
-			// List <Customer> customerUpdate = customerRepository.findBycustomerId(customerId);
-			
-
-
-			/*
-			List<CustomerContact> customerContactList = new ArrayList<CustomerContact>();
-			customerContactList.add(customerContact);
-			
-			customerPdfIdUpdate.setCustomerContact(customerContactList);
-			
-			customerPdfIdUpdate.setCustomerContractDetalis(customerContractDetalis);
-			*/
-			
-			customerDatabaseService.updateCostumer(customer, customerAdress, customerContact, customerContractDetalis, customerSelectionId);
-			
-			/*
-			Customer customerToUpdate = customerRepositoryImp.findBycustomerContractPdfId(customerSelectionId);
-			
-			customer.setCustomerId(customerToUpdate.getCustomerId());
-			customerAdress.setCustomer(customer);
-			customer.setCustomerAdress(customerAdress);
-			customerContact.setCustomer(customer);
-			
-			
-			List<CustomerContact> customerContactList = new ArrayList<CustomerContact>();
-			customerContactList.add(customerContact);
-			
-			customer.setCustomerContact(customerContactList);
-			
-			
-			customerContractDetalis.setCustomer(customer);
-			customer.setCustomerContractDetalis(customerContractDetalis);
-
-			customerRepository.save(customer);
-			*/
+			customerDatabaseService.updatingCustomer(customer, customerAdress, customerContact, customerContractDetalis, customerSelectionId);
 			
 			return "redirect:/customer/showingCustomers";
 		}
