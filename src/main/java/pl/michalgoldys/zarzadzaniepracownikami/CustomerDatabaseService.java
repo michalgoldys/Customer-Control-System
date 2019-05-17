bda47769f3ea3c55b85868a58a34c605f633143e
Michał Gołdys <michal.goldys@onet.pl>
2019-05-17 14:30:13 +0200

    * Implementation - Created Database service layer. -updatingCustomer -CreatingCustomer -finding methods

--- src/main/java/pl/michalgoldys/zarzadzaniepracownikami/CustomerDatabaseService.java
+++ src/main/java/pl/michalgoldys/zarzadzaniepracownikami/CustomerDatabaseService.java
@@ -10,42 +10,20 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CustomerDatabaseService {
-
-		@Autowired
-		CustomerRepositoryImp customerRepositoryImp;
 		
 		@Autowired
 		CustomerRepository customerRepository;
 		
 		@Autowired
-		CustomerAdressRepository customerAdressRepository;
-		
-		@Autowired
-		CustomerContactRepository customerContactRepository;
-		
-		@Autowired
-		CustomerContractDetalisRepository customerContractDetalisRepository;
+		CustomerRepositoryImp customerRepositoryImp;
 		
 		@Transactional
-		public void updateCostumer(Customer customer, CustomerAdress customerAdress, CustomerContact customerContact, 
+		public void updatingCustomer(Customer customer, CustomerAdress customerAdress, CustomerContact customerContact, 
 				CustomerContractDetalis customerContractDetalis, String customerSelectionId)
 		{
-			
 			Customer updatingCustomer = new Customer();
-			//CustomerAdress customerAdressToUpdate = new CustomerAdress();
 			
-			//CustomerContact customerContactToUpdate = new CustomerContact();
-			//CustomerContractDetalis customerContractDetalisToUpdate = new CustomerContractDetalis();
-			
-			updatingCustomer = customerRepositoryImp.findBycustomerContractPdfId(customerSelectionId);
-			
-			//Long customerId = updatingCustomer.getCustomerId();
-			
-			//CustomerAdress customerAdressToUpdate = customerAdressRepository.findBycustomerId(customerId);
-			
-			//CustomerContact customerContactToUpdate = customerContactRepository.findBycustomerId(customerId);
-			
-			//CustomerContractDetalis customerContractDetalisToUpdate = customerContractDetalisRepository.findBycustomerId(customerId);
+			updatingCustomer = customerFindByCustomerContractPdfId(customerSelectionId);
 			
 			updatingCustomer.setCustomerName(customer.getCustomerName());
 			updatingCustomer.setCustomerNip(customer.getCustomerNip());
@@ -62,47 +40,43 @@ public class CustomerDatabaseService {
 			updatingCustomer.setCustomerContractDetalis(customerContractDetalis);
 			customerContractDetalis.setCustomer(updatingCustomer);
 			
-			/*
+			customerRepository.save(updatingCustomer);
+		}
+		
+		@Transactional
+		public void creatingCustomer(Customer customer, CustomerAdress customerAdress, CustomerContact customerContact, 
+				CustomerContractDetalis customerContractDetalis)
+		{
 			customerAdress.setCustomer(customer);
 			customer.setCustomerAdress(customerAdress);
 			customerContact.setCustomer(customer);
 			
-			
 			List<CustomerContact> customerContactList = new ArrayList<CustomerContact>();
 			customerContactList.add(customerContact);
 			
-			customer.setCustomerContact(customerContactList);
-			
+			customer.setCustomerContact(customerContactList);		
 			
 			customerContractDetalis.setCustomer(customer);
 			customer.setCustomerContractDetalis(customerContractDetalis);
 			
-			/*
-			customerToUpdate.setCustomerName(customer.getCustomerName());
-			customerToUpdate.setCustomerNip(customer.getCustomerNip());
-			customerToUpdate.setCustomerAdress(customerAdress);
-			customerToUpdate.setCustomerContractDetalis(customerContractDetalis);
-			
-			
-			customerAdressToUpdate.setCustomerCity(customerAdress.getCustomerCity());
-			customerAdressToUpdate.setCustomerStreet(customerAdress.getCustomerStreet());
-			customerAdressToUpdate.setCustomerZipCode(customerAdress.getCustomerZipCode());
+			customerRepository.save(customer);
+		}
 		
-			customerContactToUpdate.setCustomerContactName(customerContact.getCustomerContactName());
-			customerContactToUpdate.setCustomerContactSurname(customerContact.getCustomerContactSurname());
-			customerContactToUpdate.setCustomerContactPhoneNumber(customerContact.getCustomerContactPhoneNumber());
-			customerContactToUpdate.setCustomerContactMailAdress(customerContact.getCustomerContactMailAdress());
-			
-			customerContractDetalisToUpdate.setCustomerPlSubstripctions(customerContractDetalis.getCustomerPlSubstripctions());
-			customerContractDetalisToUpdate.setCustomerPlUeSubstripctions(customerContractDetalis.getCustomerPlUeSubstripctions());
-			customerContractDetalisToUpdate.setCustomerRuSubscriptions(customerContractDetalis.getCustomerRuSubscriptions());
-			*/
-			
-			customerRepository.save(updatingCustomer);
-			
-			//customerAdressRepository.save(customerAdressToUpdate);
-			//customerContactRepository.save(customerContactToUpdate);
-			//customerContractDetalisRepository.save(customerContractDetalisToUpdate);
-			
+		@Transactional
+		public List<Customer> findAllCustomers()
+		{
+			return(customerRepository.findAll());
+		}
+		
+		@Transactional
+		public List<Customer> listFindByCustomerContractPdfId(String customerSelectionId)
+		{
+			return(customerRepository.findBycustomerContractPdfId(customerSelectionId));
+		}
+		
+		@Transactional
+		public Customer customerFindByCustomerContractPdfId(String customerSelectionId)
+		{
+			return(customerRepositoryImp.findBycustomerContractPdfId(customerSelectionId));
 		}
 }
