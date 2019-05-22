e9c27528179b0e133bff17269d496dd2109a9989
Michał Gołdys <michal.goldys@onet.pl>
2019-05-22 15:44:56 +0200

    * Implementation - Created getCurrentTime method.

--- src/main/java/pl/michalgoldys/zarzadzaniepracownikami/CustomerService.java
+++ src/main/java/pl/michalgoldys/zarzadzaniepracownikami/CustomerService.java
@@ -1,9 +1,18 @@
 package pl.michalgoldys.zarzadzaniepracownikami;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class CustomerService {
 
-		
+		public String getCurrentDate() {
+			Date currentDate = new Date();
+		      SimpleDateFormat ft = 
+		      new SimpleDateFormat ("dd.MM.yyyy");
+		      
+			return (ft.format(currentDate));
+		}
 }
