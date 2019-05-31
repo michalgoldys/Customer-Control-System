540acf1f8bfea7e24eae05734b48b5f7ed357981
Michał Gołdys <michal.goldys@onet.pl>
2019-05-31 15:41:00 +0200

    * Update - Added failureUrl

--- src/main/java/pl/michalgoldys/zarzadzaniepracownikami/WebSecurityConfig.java
+++ src/main/java/pl/michalgoldys/zarzadzaniepracownikami/WebSecurityConfig.java
@@ -24,7 +24,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	                .formLogin()
 	                .loginPage("/login.html")
 	                .loginProcessingUrl("/doLogin")
-	                .defaultSuccessUrl("/customer/customerMenu", true);
+	                .defaultSuccessUrl("/customer/customerMenu", true)
+	        		.failureUrl("/login.html?error=true");
 
 	 }
 	 
