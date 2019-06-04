faca615ee6a5f96933453398ae0ef2a0502caa7b
Michał Gołdys <michal.goldys@onet.pl>
2019-06-04 15:10:41 +0200

    * Update - Changed from List to Class type, methods in CustomerContractDetalis repository

--- src/main/java/pl/michalgoldys/zarzadzaniepracownikami/CustomerContractDetalisRepository.java
+++ src/main/java/pl/michalgoldys/zarzadzaniepracownikami/CustomerContractDetalisRepository.java
@@ -1,10 +1,9 @@
 package pl.michalgoldys.zarzadzaniepracownikami;
 
-import java.util.List;
-
 import org.springframework.data.repository.Repository;
 
 public interface CustomerContractDetalisRepository extends Repository<CustomerContractDetalis, Long>{
 	
-	List<CustomerContractDetalis> save(CustomerContractDetalis customerContractDetalis);
+	CustomerContractDetalis save(CustomerContractDetalis customerContractDetalis);
+	CustomerContractDetalis findBycustomerContractDetalisId(Long customerContractDetalisId);
 }
