4014136f3ded82b394126d11fb92247e5bd99a86
Michał Gołdys <michal.goldys@onet.pl>
2019-06-05 15:41:52 +0200

    Moved buissnes logic from controller to service class

--- src/main/java/pl/michalgoldys/zarzadzaniepracownikami/CustomerService.java
+++ src/main/java/pl/michalgoldys/zarzadzaniepracownikami/CustomerService.java
@@ -1,7 +1,9 @@
 package pl.michalgoldys.zarzadzaniepracownikami;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.stereotype.Service;
 
@@ -14,5 +16,46 @@ public class CustomerService {
 		      new SimpleDateFormat ("dd.MM.yyyy hh:mm:ss");
 		      
 			return (ft.format(currentDate));
+		}
+		
+		public List<Integer> sumingSubscirptionPlUeRu(List<Customer> customerList)
+		{
+			List<Integer> customerSubstripctionsSum = new ArrayList<Integer>();
+			
+			for(Customer customer : customerList)
+			{
+				int subSum = 0;
+				
+					if(customer.getCustomerContractDetalis().getCustomerPlSubstripctions() != null)
+					{
+						subSum += customer.getCustomerContractDetalis().getCustomerPlSubstripctions();
+					}
+					if(customer.getCustomerContractDetalis().getCustomerPlUeSubstripctions() != null)
+					{
+						subSum += customer.getCustomerContractDetalis().getCustomerPlUeSubstripctions();
+					}
+					if(customer.getCustomerContractDetalis().getCustomerRuSubscriptions() != null)
+					{
+						subSum += customer.getCustomerContractDetalis().getCustomerRuSubscriptions();
+					}
+				customerSubstripctionsSum.add(subSum);
+			}
+			
+			return customerSubstripctionsSum;
+		}
+		
+		public Boolean isActivationCheckboxDisabled(List<Customer> customerList)
+		{
+			boolean isDisabled = false;
+			
+			for(Customer customer : customerList) {
+				
+				if(customer.getCustomerActivationDate() != null && customer.getCustomerActivationDate().length() > 0 
+						&& customer.getCustomerDeactivationDate() != null && customer.getCustomerDeactivationDate().length() > 0){
+					
+					isDisabled = true;
+				}
+			}
+			return isDisabled;
 		}
 }
