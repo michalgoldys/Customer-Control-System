b53e975957acd1e76bf80e084ab485ca5ae489b9
Michał Gołdys <michal.goldys@onet.pl>
2019-06-06 09:39:06 +0200

    Added methods for suming income and costs

--- src/main/java/pl/michalgoldys/zarzadzaniepracownikami/CustomerService.java
+++ src/main/java/pl/michalgoldys/zarzadzaniepracownikami/CustomerService.java
@@ -44,7 +44,7 @@ public class CustomerService {
 			return customerSubstripctionsSum;
 		}
 		
-		public Boolean isActivationCheckboxDisabled(List<Customer> customerList)
+		public Boolean settingActivationCheckbox(List<Customer> customerList)
 		{
 			boolean isDisabled = false;
 			
@@ -57,5 +57,69 @@ public class CustomerService {
 				}
 			}
 			return isDisabled;
+		}
+		
+		public List<Integer> sumOfIncomeBySubscriptions(List<Customer> customerList)
+		{
+			List<Integer> sumOfIncomeBySubscriptions = new ArrayList<Integer>();
+			
+			for(Customer customer : customerList)
+			{
+				int incomeSum = 0;
+				
+				if(customer.getCustomerContractDetalis().getCustomerPlSubstripctions() != null && customer.getCustomerContractDetalis().getCustomerPlFee() != null
+						&& customer.getCustomerContractDetalis().getCustomerPlSubstripctions() > 0 && customer.getCustomerContractDetalis().getCustomerPlFee() > 0)
+				{
+					incomeSum += customer.getCustomerContractDetalis().getCustomerPlSubstripctions() * customer.getCustomerContractDetalis().getCustomerPlFee();
+				}
+				if(customer.getCustomerContractDetalis().getCustomerPlUeSubstripctions() != null && customer.getCustomerContractDetalis().getCustomerPlUeFee() != null
+						&& customer.getCustomerContractDetalis().getCustomerPlUeSubstripctions() > 0 && customer.getCustomerContractDetalis().getCustomerPlUeFee() > 0)
+				{
+					incomeSum += customer.getCustomerContractDetalis().getCustomerPlUeSubstripctions() * customer.getCustomerContractDetalis().getCustomerPlUeFee();
+				}
+				if(customer.getCustomerContractDetalis().getCustomerRuSubscriptions() != null && customer.getCustomerContractDetalis().getCustomerRuFee() != null
+						&& customer.getCustomerContractDetalis().getCustomerRuSubscriptions() > 0 && customer.getCustomerContractDetalis().getCustomerRuFee() > 0)
+				{
+					incomeSum += customer.getCustomerContractDetalis().getCustomerRuSubscriptions() * customer.getCustomerContractDetalis().getCustomerRuFee();
+				}
+				sumOfIncomeBySubscriptions.add(incomeSum);
+			}
+			
+			return sumOfIncomeBySubscriptions;
+		}
+		
+		public List<Double> sumOfCosts(List<Customer> customerList)
+		{
+			List<Double> sumOfCosts = new ArrayList<Double>();
+			
+			int serverCost = 10;
+			double telematicCardCost = 2.50;
+			
+			for(Customer customer : customerList) 
+			{
+				double costSum = 0;
+				
+				if(customer.getCustomerContractDetalis().getCustomerPlSubstripctions() > 0 && 
+						customer.getCustomerContractDetalis().getCustomerPlSubstripctions() != null)
+				{
+					costSum += customer.getCustomerContractDetalis().getCustomerPlSubstripctions() * serverCost;
+					costSum += customer.getCustomerContractDetalis().getCustomerPlSubstripctions() * telematicCardCost;
+				}
+				if(customer.getCustomerContractDetalis().getCustomerPlUeSubstripctions() > 0 && 
+						customer.getCustomerContractDetalis().getCustomerPlUeSubstripctions() != null)
+				{
+					costSum += customer.getCustomerContractDetalis().getCustomerPlUeSubstripctions() * serverCost;
+					costSum += customer.getCustomerContractDetalis().getCustomerPlUeSubstripctions() * telematicCardCost;
+				}
+				if(customer.getCustomerContractDetalis().getCustomerRuSubscriptions() > 0 && 
+						customer.getCustomerContractDetalis().getCustomerRuSubscriptions() != null)
+				{
+					costSum += customer.getCustomerContractDetalis().getCustomerRuSubscriptions() * serverCost;
+					costSum += customer.getCustomerContractDetalis().getCustomerRuSubscriptions() * telematicCardCost;
+				}
+				sumOfCosts.add(costSum);
+			}
+			
+			return sumOfCosts;
 		}
 }
