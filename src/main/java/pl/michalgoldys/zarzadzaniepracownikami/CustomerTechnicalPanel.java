dee2d4735095d7671780718aabad612f8a53dc0b
Michał Gołdys <michal.goldys@onet.pl>
2019-06-11 14:59:01 +0200

    Created new class as a object for technical issues that user can add to customers

---
+++ src/main/java/pl/michalgoldys/zarzadzaniepracownikami/CustomerTechnicalPanel.java
@@ -0,0 +1,58 @@
+package pl.michalgoldys.zarzadzaniepracownikami;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Entity(name="CUSTOMER_TECHNICAL_PANEL")
+public class CustomerTechnicalPanel {
+		
+		@Id
+		@GeneratedValue(strategy=GenerationType.AUTO)
+		private Long customerTechnicalPanelId;
+		
+		@Column(name="customer_technical_issue_occour_date")
+		private String customerTechnicalIssueOccourDate;
+		
+		@Column(name="customer_technical_issue_describtion")
+		private String customerTechnicalIssueDescribtion;
+		
+		@Autowired
+		@ManyToOne
+		@JoinColumn(name = "customerId")
+		Customer customer;
+		
+		protected CustomerTechnicalPanel() {
+			
+		}
+
+		public Long getCustomerTechnicalPanelId() {
+			return customerTechnicalPanelId;
+		}
+
+		public void setCustomerTechnicalPanelId(Long customerTechnicalPanelId) {
+			this.customerTechnicalPanelId = customerTechnicalPanelId;
+		}
+
+		public String getCustomerTechnicalIssueOccourDate() {
+			return customerTechnicalIssueOccourDate;
+		}
+
+		public void setCustomerTechnicalIssueOccourDate(String customerTechnicalIssueOccourDate) {
+			this.customerTechnicalIssueOccourDate = customerTechnicalIssueOccourDate;
+		}
+
+		public String getCustomerTechnicalIssueDescribtion() {
+			return customerTechnicalIssueDescribtion;
+		}
+
+		public void setCustomerTechnicalIssueDescribtion(String customerTechnicalIssueDescribtion) {
+			this.customerTechnicalIssueDescribtion = customerTechnicalIssueDescribtion;
+		}
+}
