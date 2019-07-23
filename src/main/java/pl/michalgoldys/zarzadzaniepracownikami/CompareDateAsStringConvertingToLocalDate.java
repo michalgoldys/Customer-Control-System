c7ce59648b5af3f51c6ff2c61095a045e7841543
Michał Gołdys <michal.goldys@onet.pl>
2019-07-23 15:33:50 +0200

    Changed type from CustomerTechnicalPanel to String and added DateTimeFormatter for parsing string to date puropses and sorting.

--- src/main/java/pl/michalgoldys/zarzadzaniepracownikami/CompareDateAsStringConvertingToLocalDate.java
+++ src/main/java/pl/michalgoldys/zarzadzaniepracownikami/CompareDateAsStringConvertingToLocalDate.java
@@ -1,11 +1,15 @@
 package pl.michalgoldys.zarzadzaniepracownikami;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Comparator;
 
-public class CustomerTechnicalPanelSortByCustomerTechnicalIssueOccourDate implements Comparator<CustomerTechnicalPanel> {
+public class CompareDateAsStringConvertingToLocalDate implements Comparator<String> {
 
+	DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+	
 	@Override
-	public int compare(CustomerTechnicalPanel o1, CustomerTechnicalPanel o2) {
-		return o1.getCustomerTechnicalIssueOccourDate().compareTo(o2.getCustomerTechnicalIssueOccourDate());
+	public int compare(String o1, String o2) {
+		return LocalDate.parse(o1, formatter).compareTo(LocalDate.parse(o2, formatter));
 	}
 }
