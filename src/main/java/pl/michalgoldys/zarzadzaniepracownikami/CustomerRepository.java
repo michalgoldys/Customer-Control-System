44160f4a3e9f83079557a18104fa2d2a5d1f47a8
Michał Gołdys <michal.goldys@onet.pl>
2019-07-26 15:04:02 +0200

    Added new method with Pageable interface for sorting, paging

--- src/main/java/pl/michalgoldys/zarzadzaniepracownikami/CustomerRepository.java
+++ src/main/java/pl/michalgoldys/zarzadzaniepracownikami/CustomerRepository.java
@@ -2,12 +2,15 @@ package pl.michalgoldys.zarzadzaniepracownikami;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long>{
 		
 		List<Customer> findBycustomerName(String customerName);
 		List<Customer> findBycustomerId(Long customerId);
+		Page<Customer> findAll(Pageable pageable);
 		List<Customer> findAll();
 		List<Customer> findBycustomerContractPdfId(String customerContractPdfId);
 }
