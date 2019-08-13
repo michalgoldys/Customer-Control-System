8a8e8144da63bcdb17f64046bfcf3d447019cbd0
Michał <Godys>
2019-08-13 16:05:09 +0200

    Changed name of interface for more clean meaning

--- src/main/java/pl/michalgoldys/zarzadzaniepracownikami/CustomerJpaRepository.java
+++ src/main/java/pl/michalgoldys/zarzadzaniepracownikami/CustomerJpaRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CustomerRepository extends JpaRepository<Customer, Long>{
+public interface CustomerJpaRepository extends JpaRepository<Customer, Long>{
 		
 		List<Customer> findBycustomerName(String customerName);
 		List<Customer> findBycustomerId(Long customerId);
