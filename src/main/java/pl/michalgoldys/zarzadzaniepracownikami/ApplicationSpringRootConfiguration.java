fa5c5f0235c3da4b0c20b2dd627b644d5841aa0c
Michał Gołdys <michal.goldys@onet.pl>
2019-10-17 15:19:11 +0200

    Added @deprecated annotion for not used class

--- src/main/java/pl/michalgoldys/zarzadzaniepracownikami/ApplicationSpringRootConfiguration.java
+++ src/main/java/pl/michalgoldys/zarzadzaniepracownikami/ApplicationSpringRootConfiguration.java
@@ -2,6 +2,7 @@ package pl.michalgoldys.zarzadzaniepracownikami;
 
 import org.springframework.context.annotation.Configuration;
 
+@Deprecated
 @Configuration
 public class ApplicationSpringRootConfiguration {
 
