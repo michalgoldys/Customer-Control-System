28b6d8ca3cfd8a5b4ede0770e10f522a135ad258
Michał Gołdys <michal.goldys@onet.pl>
2019-12-17 22:21:04 +0100

    Fixed Configuration File
    Fixed and revert back configuration file where configuration
    for Hibernate were removed by mistake

--- src/main/java/pl/michalgoldys/zarzadzaniepracownikami/ApplicationConfiguration.java
+++ src/main/java/pl/michalgoldys/zarzadzaniepracownikami/ApplicationConfiguration.java
@@ -1,13 +1,21 @@
 package pl.michalgoldys.zarzadzaniepracownikami;
+import javax.persistence.EntityManagerFactory;
 import javax.servlet.ServletContext;
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Description;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.support.PersistenceAnnotationBeanPostProcessor;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 import org.thymeleaf.templatemode.TemplateMode;
@@ -16,55 +24,55 @@ import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 @Configuration
 @ComponentScan
 public class ApplicationConfiguration  {
-	
+
 	@Bean
 	public DataSource dataSource() {
 		return DataSourceBuilder
-		.create()
-		.username("root")
-		.password("fajka12!@")
-		.url("jdbc:mysql://localhost:3306/zarzadzaniepracownikamidb?createDatabaseIfNotExist=true&allowPublicKeyRetrieval=true&useSSL=false&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC")
-		.driverClassName("com.mysql.jdbc.Driver")
-		.build();
+				.create()
+				.username("root")
+				.password("fajka12!@")
+				.url("jdbc:mysql://localhost:3306/zarzadzaniepracownikamidb?createDatabaseIfNotExist=true&allowPublicKeyRetrieval=true&useSSL=false&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC")
+				.driverClassName("com.mysql.jdbc.Driver")
+				.build();
 	}
-	
+
 	@Bean
 	@Description("Thymeleaf Template Resolver")
 	public ServletContextTemplateResolver templateResolver(final ServletContext servletContext) {
-	    ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(servletContext);
-	    templateResolver.setPrefix("/resources/templates/");
-	    templateResolver.setSuffix(".html");
-	    templateResolver.setTemplateMode(TemplateMode.HTML);
-	    templateResolver.setCacheable(false);
-	 
-	    return templateResolver;
+		ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(servletContext);
+		templateResolver.setPrefix("/resources/templates/");
+		templateResolver.setSuffix(".html");
+		templateResolver.setTemplateMode(TemplateMode.HTML);
+		templateResolver.setCacheable(false);
+
+		return templateResolver;
 	}
-	
+
 	@Bean
 	@Description("Thymeleaf Template Engine")
 	public SpringTemplateEngine templateEngine(final ServletContext servletContext) {
-	    SpringTemplateEngine templateEngine = new SpringTemplateEngine();
-	    templateEngine.setTemplateResolver(templateResolver(servletContext));
-	    templateEngine.setTemplateEngineMessageSource(messageSource());
-	    templateEngine.setEnableSpringELCompiler(true);
-	    return templateEngine;
+		SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+		templateEngine.setTemplateResolver(templateResolver(servletContext));
+		templateEngine.setTemplateEngineMessageSource(messageSource());
+		templateEngine.setEnableSpringELCompiler(true);
+		return templateEngine;
 	}
-	
+
 	@Bean
 	@Description("Thymeleaf View Resolver")
 	public ThymeleafViewResolver viewResolver(final ServletContext servletContext) {
-    ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
-    viewResolver.setTemplateEngine(templateEngine(servletContext));
-    viewResolver.setOrder(1);
-    return viewResolver;
+		ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+		viewResolver.setTemplateEngine(templateEngine(servletContext));
+		viewResolver.setOrder(1);
+		return viewResolver;
 	}
-	
+
 	@Bean
 	@Description("Spring Message Resolver")
 	public ResourceBundleMessageSource messageSource() {
-	    ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-	    messageSource.setBasename("messages");
-	    return messageSource;
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("messages");
+		return messageSource;
 	}
 	/*
 	@Bean
@@ -75,4 +83,12 @@ public class ApplicationConfiguration  {
 		return irvr;
 	}
 	*/
+	@Bean
+	public HibernateJpaVendorAdapter settingHibernateJpaVendorAdapter() {
+		HibernateJpaVendorAdapter hjva = new HibernateJpaVendorAdapter();
+		hjva.setShowSql(true);
+		hjva.setDatabasePlatform("org.hibernate.dialect.MySQL5Dialect");
+		hjva.setGenerateDdl(true);
+		return hjva;
+	}
 }
