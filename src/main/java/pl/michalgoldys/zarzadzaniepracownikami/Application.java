b69040cb4ef2e415416688a707d64d3bea5b8e45
Michał Gołdys <michal.goldys@onet.pl>
2019-12-20 22:19:04 +0100

    Rebuild spring annotions, move from another class

--- src/main/java/pl/michalgoldys/zarzadzaniepracownikami/Application.java
+++ src/main/java/pl/michalgoldys/zarzadzaniepracownikami/Application.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @EnableTransactionManagement
 @EnableJpaRepositories
 @SpringBootApplication
-@EntityScan(basePackages = {"pl.michalgoldys"}) 
+@EntityScan(basePackages = {"pl.michalgoldys"})
+@EnableWebMvc
 public class Application {
 
 	public static void main(String[] args) {
