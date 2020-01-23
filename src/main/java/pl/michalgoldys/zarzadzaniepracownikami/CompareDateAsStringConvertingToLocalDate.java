982428d6e4a25c0a3dd7c77d29094ec0b1a19837
Michał Gołdys <michal.goldys@onet.pl>
2020-01-23 15:07:37 +0100

    Implementation of DateService and refactor comparator code

--- src/main/java/pl/michalgoldys/zarzadzaniepracownikami/CompareDateAsStringConvertingToLocalDate.java
+++ src/main/java/pl/michalgoldys/zarzadzaniepracownikami/CompareDateAsStringConvertingToLocalDate.java
@@ -6,10 +6,10 @@ import java.util.Comparator;
 
 public class CompareDateAsStringConvertingToLocalDate implements Comparator<String> {
 
-	DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-	
-	@Override
-	public int compare(String o1, String o2) {
-		return LocalDate.parse(o1, formatter).compareTo(LocalDate.parse(o2, formatter));
-	}
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+    @Override
+    public int compare(String o1, String o2) {
+        return LocalDate.parse(o1, formatter).compareTo(LocalDate.parse(o2, formatter));
+    }
 }
