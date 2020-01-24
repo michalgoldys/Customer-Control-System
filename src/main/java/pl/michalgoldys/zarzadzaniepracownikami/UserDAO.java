54e7bbe48aa3f8245a594445fa32b727c77da4b0
Michał Gołdys <michal.goldys@onet.pl>
2020-01-24 14:55:02 +0100

    Create required methods

--- src/main/java/pl/michalgoldys/zarzadzaniepracownikami/UserDAO.java
+++ src/main/java/pl/michalgoldys/zarzadzaniepracownikami/UserDAO.java
@@ -1,4 +1,10 @@
 package pl.michalgoldys.zarzadzaniepracownikami;
 
-public interface UserDAO {
+import org.springframework.data.repository.Repository;
+
+@org.springframework.stereotype.Repository
+public interface UserDAO extends Repository<User, Long> {
+
+    User findByusername(String username);
+    void save (User user);
 }
