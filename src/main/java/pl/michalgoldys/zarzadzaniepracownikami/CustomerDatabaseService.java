9e1812afe80c27ddcac03d4854967f46c1f059ce
Michał Gołdys <michal.goldys@onet.pl>
2020-01-31 14:46:13 +0100

    Removed unused implementations

--- src/main/java/pl/michalgoldys/zarzadzaniepracownikami/CustomerDatabaseService.java
+++ src/main/java/pl/michalgoldys/zarzadzaniepracownikami/CustomerDatabaseService.java
@@ -9,10 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Service
-public class CustomerDatabaseService implements CustomerDatabaseServiceInterface, DatabaseOperationInterface{
-
-		@Autowired
-		DateServiceImpl dateService;
+public class CustomerDatabaseService implements DatabaseOperationInterface{
 
 		@Autowired
 		CustomerRepository customerRepository;
@@ -20,108 +17,6 @@ public class CustomerDatabaseService implements CustomerDatabaseServiceInterface
 		@Autowired
 		CustomerRepositoryImp customerRepositoryImp;
 
-		@Autowired
-		CustomerContractDetailsRepository customerContractDetailsRepository;
-		
-		@Override
-		public void updatingCustomer(Customer customer, CustomerAddress customerAddress, CustomerContact customerContact, String customerSelectionId)
-		{
-			Customer updatingCustomer = new Customer();
-			
-			updatingCustomer = customerFindByCustomerContractPdfId(customerSelectionId);
-
-			Boolean isActive = customer.getCustomerIsActive();
-			String activationDate = customer.getCustomerActivationDate();
-			String deactivationDate = customer.getCustomerDeactivationDate();
-			
-			if (isActive && activationDate.length() == 0)
-			{
-				customer.setCustomerActivationDate(dateService.getCurrentDate());
-			}
-			
-			if (!isActive && activationDate != null && activationDate.length() > 0 && deactivationDate.length() == 0)
-			{
-				customer.setCustomerDeactivationDate(dateService.getCurrentDate());
-			}
-			
-			updatingCustomer.setCustomerName(customer.getCustomerName());
-			updatingCustomer.setCustomerNip(customer.getCustomerNip());
-			updatingCustomer.setCustomerIsActive(customer.getCustomerIsActive());
-			updatingCustomer.setCustomerActivationDate(customer.getCustomerActivationDate());
-			updatingCustomer.setCustomerDeactivationDate(customer.getCustomerDeactivationDate());
-
-			updatingCustomer.setCustomerAddress(customerAddress);
-			customerAddress.setCustomer(updatingCustomer);
-			
-			List<CustomerContact> customerContactList = new ArrayList<CustomerContact>();
-			customerContactList.add(customerContact);
-			
-			updatingCustomer.setCustomerContact(customerContactList);
-			customerContact.setCustomer(updatingCustomer);
-			
-			customerRepository.save(updatingCustomer);
-		}
-		
-		@Override
-		public void creatingCustomer(Customer customer, CustomerAddress customerAddress, CustomerContact customerContact,
-									 CustomerContractDetails customerContractDetails)
-		{
-			customerAddress.setCustomer(customer);
-			customer.setCustomerAddress(customerAddress);
-			customerContact.setCustomer(customer);
-			
-			List<CustomerContact> customerContactList = new ArrayList<CustomerContact>();
-			customerContactList.add(customerContact);
-			
-			customer.setCustomerContact(customerContactList);		
-			
-			customerContractDetails.setCustomer(customer);
-			customer.setCustomerContractDetails(customerContractDetails);
-			
-			customerRepository.save(customer);
-		}
-		
-		@Override
-		public void updatingCustomerBilling(CustomerContractDetails customerContractDetails)
-		{
-			Long selection = customerContractDetails.getCustomerContractDetailsId();
-				
-			CustomerContractDetails customerBillingDetails = customerContractDetailsRepository.findBycustomerContractDetailsId(selection);
-			
-			customerBillingDetails.setCustomerPlFee(customerContractDetails.getCustomerPlFee());
-			customerBillingDetails.setCustomerPlSubscriptions(customerContractDetails.getCustomerPlSubscriptions());
-			
-			customerBillingDetails.setCustomerPlUeFee(customerContractDetails.getCustomerPlUeFee());
-			customerBillingDetails.setCustomerPlUeSubscriptions(customerContractDetails.getCustomerPlUeSubscriptions());
-			
-			customerBillingDetails.setCustomerRuFee(customerContractDetails.getCustomerRuFee());
-			customerBillingDetails.setCustomerRuSubscriptions(customerContractDetails.getCustomerRuSubscriptions());
-			
-			customerBillingDetails.setCustomerEinvoiceAgreement(customerContractDetails.getCustomerEinvoiceAgreement());
-			
-			customerContractDetailsRepository.save(customerBillingDetails);
-		}
-		
-		@Override
-		public void creatingCustomerTechnicalPanelEntity(Customer selectedCustomer, CustomerTechnicalPanel customerTechnicalPanel)
-		{
-			
-			CustomerTechnicalPanel newCustomerTechnicalPanel = new CustomerTechnicalPanel();
-			
-			newCustomerTechnicalPanel.setCustomerTechnicalIssueDescribtion(customerTechnicalPanel.getCustomerTechnicalIssueDescribtion());
-			newCustomerTechnicalPanel.setCustomerTechnicalIssueOccourDate(customerTechnicalPanel.getCustomerTechnicalIssueOccourDate());
-			
-			newCustomerTechnicalPanel.setCustomer(selectedCustomer);
-			
-			List<CustomerTechnicalPanel> customerTechnicalPanelList = new ArrayList<CustomerTechnicalPanel>();
-			customerTechnicalPanelList.add(newCustomerTechnicalPanel);
-			
-			selectedCustomer.setCustomerTechnicalPanel(customerTechnicalPanelList);
-			
-			
-			customerRepository.save(selectedCustomer);
-		}
-		
 		@Override
 		public List<Customer> findAllCustomers()
 		{
@@ -133,13 +28,13 @@ public class CustomerDatabaseService implements CustomerDatabaseServiceInterface
 		{
 			return(customerRepository.findAll(pageRequest));
 		}
-		
+
 		@Override
 		public List<Customer> listFindByCustomerContractPdfId(String customerSelectionId)
 		{
 			return(customerRepository.findBycustomerContractPdfId(customerSelectionId));
 		}
-		
+
 		@Override
 		public Customer customerFindByCustomerContractPdfId(String customerSelectionId)
 		{
