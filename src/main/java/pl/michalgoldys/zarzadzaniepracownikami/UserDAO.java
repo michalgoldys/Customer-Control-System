7ad3f1b401b36377e77f2a237a26dbaf5d49fcd9
Michał Gołdys <michal.goldys@onet.pl>
2020-02-12 11:07:16 +0100

    Add method for query all users

--- src/main/java/pl/michalgoldys/zarzadzaniepracownikami/UserDAO.java
+++ src/main/java/pl/michalgoldys/zarzadzaniepracownikami/UserDAO.java
@@ -2,9 +2,12 @@ package pl.michalgoldys.zarzadzaniepracownikami;
 
 import org.springframework.data.repository.Repository;
 
+import java.util.Collection;
+
 @org.springframework.stereotype.Repository
 public interface UserDAO extends Repository<User, Long> {
 
+    Collection findAll();
     User findByusername(String username);
     void save (User user);
 }
