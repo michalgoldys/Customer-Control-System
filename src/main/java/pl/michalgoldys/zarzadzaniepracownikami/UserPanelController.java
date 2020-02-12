011c3934eaf8c12b807bc922ef50c8baf0d65fdd
Michał Gołdys <michal.goldys@onet.pl>
2020-02-12 11:08:04 +0100

    Add handler method for view

--- src/main/java/pl/michalgoldys/zarzadzaniepracownikami/UserPanelController.java
+++ src/main/java/pl/michalgoldys/zarzadzaniepracownikami/UserPanelController.java
@@ -1,9 +1,22 @@
 package pl.michalgoldys.zarzadzaniepracownikami;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Slf4j
 @Controller
 public class UserPanelController {
+
+    @Autowired
+    FindUsersReturnAsListServiceImpl findUsersReturnAsListService;
+
+    @GetMapping(value = "/customer/userscontrolpanel")
+    String userControlPanelView(Model model) {
+        log.info("Getting users from service..");
+        model.addAttribute("userList", findUsersReturnAsListService.findAll());
+        return "userscontrolpanel.html";
+    }
 }
