824f0c30d961a6d62772096107955092036adc04
Michał Gołdys <michal.goldys@onet.pl>
2020-02-14 13:58:14 +0100

    Polishing imports

--- src/main/java/pl/michalgoldys/zarzadzaniepracownikami/FindUsersReturnAsListServiceImpl.java
+++ src/main/java/pl/michalgoldys/zarzadzaniepracownikami/FindUsersReturnAsListServiceImpl.java
@@ -6,8 +6,6 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @Slf4j
