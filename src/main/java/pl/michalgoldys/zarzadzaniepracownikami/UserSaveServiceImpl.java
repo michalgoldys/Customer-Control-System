4c20ccd161ffa48ee35a8f420aa78f95cd76067c
Michał Gołdys <michal.goldys@onet.pl>
2020-02-21 10:02:34 +0100

    Optimize imports

--- src/main/java/pl/michalgoldys/zarzadzaniepracownikami/UserSaveServiceImpl.java
+++ src/main/java/pl/michalgoldys/zarzadzaniepracownikami/UserSaveServiceImpl.java
@@ -2,7 +2,6 @@ package pl.michalgoldys.zarzadzaniepracownikami;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
